a2257d677af416237c379fa2f7e474159eb12730
José Robério <jose.silva2399@etec.sp.gov.br>
2025-09-18 18:57:09 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const palavras = require('../API-Filtro-Palavras/impropias.json');
+
+
+app.get('/verificacao', (req, res) =>  {
+    const valor = req.query.value; 
+
+    const proibida = palavras.find(p => valor.includes(p));
+
+    const simbolos = /[@!#$%&*]/;
+    const tmsimbol = simbolos.test(valor);
+
+    if (proibida || tmsimbol) {
+        return res.send (`Palavra não permitida encontrada. Tente Novamente!`);
+    }
+    res.send ("Enviado com sucesso")
+});
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+})
